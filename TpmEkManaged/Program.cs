a35d999db97f13f3cb0656adc077fe4e388f075b
Dan Griffin <dan@jwsecure.com>
2016-02-26 10:28:50 -0800

    Add C# program that computes the same public key hash value (starting with a certificate).

---
+++ TpmEkManaged/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TpmEkManaged
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X509Certificate2 cert = new X509Certificate2(args[0]);
+            SHA256 sha = new SHA256CryptoServiceProvider();
+            byte[] result = sha.ComputeHash(cert.GetPublicKey());
+            SoapHexBinary shb = new SoapHexBinary(result);
+            Console.WriteLine(shb.ToString());
+        }
+    }
+}
